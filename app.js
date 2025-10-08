3998a3f048b632af5523f90f67f6b030f4c2d754
Daniel <dk807@live.mdx.ac.uk>
2025-10-08 21:38:43 +0100

    fix issues and typos in app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const createLogStream = fs.createWriteStream(
 )
 
 app.use(morgan('combined', { stream: createLogStream }));
-app.use('/puclic', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
@@ -35,7 +35,9 @@ app.use(imageANDLogsRoutes);
 app.use('/api', lessonsRoutes);
 
 app.use((error, req, res, next) => {
-    res.status(404).send('Error: ' + error.message);
+    res.status(error.statusCode).send('Error: ' + error.message);
 })
 
-app.listen(process.env.PORT || 80, console.log(`Server is running on port ${process.env.PORT}`));
+app.listen(process.env.PORT || 80, '0.0.0.0', () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
