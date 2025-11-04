1d9c7bd967a30a9c44167b34de4530443fece919
Daniel <dk807@live.mdx.ac.uk>
2025-11-04 01:36:30 +0000

    fixed the local port number of my frontend

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 app.set('json spaces', 3);
 
 app.use(cors({
-  origin: ["https://danielk111.github.io/full-stack-after-school-classes-client/", "http://localhost:5173"],
+  origin: ["https://danielk111.github.io/full-stack-after-school-classes-client/", "http://localhost:5501"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
